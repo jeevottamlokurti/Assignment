f27d8558352a1529c926ff407149fbf5b9a06090
jeevottamlokurti <jeevottamlokurti@gmail.com>
2022-10-24 19:42:46 +0530

    Updated prefix url to /languages and added error handling.
    for POST,PUT and DELETE updated the services to handle errors like id not found

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger);
-app.use(router);
+app.use("/languages",router);
 app.use(errorLogger);
 
 //listening on 8080 port
--- src/services/languageServices.js
+++ src/services/languageServices.js
@@ -6,17 +6,25 @@ exports.fetchCount = function (req, res, next) {
         function (err, client) {
             if (err) {
                 next(err)
+                res.status(500).json({
+                    success: false,
+                    message: err.sqlMessage||" Some error occured!",
+                })
             } else {
                 client.query('SELECT count(*) FROM programming_languages', function (err, rows) {
                     // And done with the connection.
                     if (err) {
                         console.log('Query Error');
                         next(err);
-                    }
-                    
+                        res.status(500).json({
+                            success: false,
+                            message: err.sqlMessage||" Some error occured!",
+                        })
+                    }else{
+
                     res.json(rows);
                     client.release();
-
+                    }
                     // Don't use the connection here, it has been returned to the pool.
                 });
             }
@@ -27,18 +35,26 @@ exports.fetchPage = function (req, res, next) {
         function (err, client) {
             if (err) {
                 next(err)
+                res.status(500).json({
+                    success: false,
+                    message: err.sqlMessage||" Some error occured!",
+                })
             } else {
                 client.query('SELECT * FROM programming_languages', function (err, rows) {
                     // And done with the connection.
                     if (err) {
                         console.log('Query Error');
                         next(err);
-                    }
+                        res.status(500).json({
+                            success: false,
+                            message: err.sqlMessage||" Some error occured!",
+                        })
+                    }else{
                     let page = req.query.page || 1;
 
                     res.json(utilities.paginator(page, rows));
                     client.release();
-
+                    }
                     // Don't use the connection here, it has been returned to the pool.
                 });
             }
@@ -49,25 +65,40 @@ exports.createEntry = function (req, res, next) {
         function (err, client) {
             if (err) {
                 next(err)
+                res.status(500).json({
+                    success: false,
+                    message: err.sqlMessage||" Some error occured!",
+                })
             } else {
-                console.log(req.body)
-                client.query(`INSERT INTO programming_languages (id,name,released_year,githut_rank,pypl_rank,tiobe_rank) VALUES (${req.body.id},"${req.body.name}",${req.body.released_year},${req.body.githut_rank},${req.body.pypl_rank},${req.body.tiobe_rank});`, function (err, rows) {
-                    // And done with the connection.
-                    if (err) {
-                        console.log('Query Error',err);
-                        next(err);
-                    }
+                try {
+                    console.log(req.body)
+                    client.query(`INSERT INTO programming_languages (id,name,released_year,githut_rank,pypl_rank,tiobe_rank) VALUES (${req.body.id},"${req.body.name}",${req.body.released_year},${req.body.githut_rank},${req.body.pypl_rank},${req.body.tiobe_rank});`, function (err, rows) {
+                        // And done with the connection.
+                        if (err) {
+                            console.log('Query Error', err.sqlMessage);
 
-                    res.json({
-                        success:true,
-                        status:200,
-                        id: req.body.id,
-                        name: req.body.name
+                            next(err);
+                            res.status(500).json({
+                                success: false,
+                                message: err.sqlMessage||" Some error occured!",
+                            })
+                        } else {
+
+                            res.json({
+                                success: true,
+                                id: req.body.id,
+                                name: req.body.name,
+                                message: "Record insertion successful!",
+                            });
+                            return client.release();
+                        }
+                        // Don't use the connection here, it has been returned to the pool.
                     });
-                    client.release();
-
-                    // Don't use the connection here, it has been returned to the pool.
-                });
+                } catch (err) {
+                    next(err)
+                    console.log(err)
+                    return
+                }
             }
         });
 }
@@ -76,33 +107,54 @@ exports.updateEntry = function (req, res, next) {
         function (err, client) {
             if (err) {
                 next(err)
+                res.status(500).json({
+                    success: false,
+                    message: err.sqlMessage||" Some error occured!",
+                })
             } else {
                 console.log(req.params.id)
 
-                client.query(`SELECT * FROM programming_languages WHERE id = ${req.params.id}`,(err,result)=>{
-                    if(err){
+                client.query(`SELECT * FROM programming_languages WHERE id = ${req.params.id}`, (err, result) => {
+                    if (err) {
                         console.log(err);
                         next(err);
-                    }
-                    console.log(result[0])
-                    client.query(`UPDATE programming_languages SET name= "${req.body.name||result[0].name}" ,released_year= ${req.body.released_year||result[0].released_year},githut_rank= ${req.body.githut_rank||result[0].githut_rank},pypl_rank= ${req.body.pypl_rank||result[0].pypl_rank},tiobe_rank= ${req.body.tiobe_rank||result[0].tiobe_rank} WHERE id= ${req.params.id}`, function (err, rows) {
-                        // And done with the connection.
-                        if (err) {
-                            console.log(err);
-                            next(err);
-                        }else{
-    
-                        res.json({
-                            success:true,
-                            status:200,
+                        res.status(500).json({
+                            success: false,
+                            message: err.sqlMessage||" Some error occured!",
+                        })
+                    } else {
+                        console.log(result[0])
+                        if(result[0]){
+                        client.query(`UPDATE programming_languages SET name= "${req.body.name || result[0].name}" ,released_year= ${req.body.released_year || result[0].released_year},githut_rank= ${req.body.githut_rank || result[0].githut_rank},pypl_rank= ${req.body.pypl_rank || result[0].pypl_rank},tiobe_rank= ${req.body.tiobe_rank || result[0].tiobe_rank} WHERE id= ${req.params.id}`, function (err, rows) {
+                            // And done with the connection.
+                            if (err) {
+                                console.log(err);
+                                next(err);
+                                res.status(500).json({
+                                    success: false,
+                                    message: err.sqlMessage||" Some error occured!",
+                                })
+                            } else {
+
+                                res.json({
+                                    success: true,
+                                    message: `Update Successfull for record id ${req.params.id}`,
+                                    status: 200,
+                                });
+                            }
+                            return client.release();
+
+                            // Don't use the connection here, it has been returned to the pool.
                         });
+                    }else{
+                        res.status(404).json({
+                            success: false,
+                            message: `Record with ID ${req.params.id} not found in DB!`,
+                        }) 
                     }
-                        client.release();
-    
-                        // Don't use the connection here, it has been returned to the pool.
-                    });
+                    }
                 })
-                
+
             }
         });
 }
@@ -111,23 +163,40 @@ exports.deleteEntry = function (req, res, next) {
         function (err, client) {
             if (err) {
                 next(err)
+                res.status(500).json({
+                    success: false,
+                    message: err.sqlMessage||" Some error occured!",
+                })
             } else {
-                client.query('DELETE FROM programming_languages WHERE id=?',[req.params.id], function (err, rows) {
+                client.query(`DELETE FROM programming_languages WHERE id=${req.params.id}`, function (err, rows) {
                     // And done with the connection.
                     if (err) {
-                        console.log('Query Error',err);
+                        console.log('Query Error', err);
                         next(err);
-                    }
-                    
+                        res.status(500).json({
+                            success: false,
+                            message: err.sqlMessage ||" Some error occured!",
+                        })
+                    }else{
 
+console.log(rows)
+if (rows && rows.affectedRows>0){
                     res.json({
-                        success:true,
-                        status:200,
+                        success: true,
+                        status: 200,
+                        message: `Record with id ${req.params.id} is deleted from DB!`,
                     });
-                    client.release();
+                }
+                else{
+                    res.status(404).json({
+                        success: false,
+                        message: `Record with id ${req.params.id} is not found in DB!`,
+                    })
+                }
+                    return client.release();
 
                     // Don't use the connection here, it has been returned to the pool.
-                });
+             } });
             }
         });
 }
